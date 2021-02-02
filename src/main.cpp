c9aff3cbb06b7f86888bba8eb9a1651fb443cb93
jesochoa <jsochoa8@gmail.com>
2021-02-02 16:37:56 +0100

    Comienzo programa

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  //digitalWrite(LED, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+}
